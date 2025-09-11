a697d7074e9e96f64076512d6523d8f1a8d6ff3d
Sophie Akoth <akothsophie9023.gmail.com>
2025-09-11 22:16:51 +0300

    add 8-square.js

---
+++ 8-square.js
@@ -0,0 +1,10 @@
+const size = parseInt(process.argv[2]);
+if(isNaN(size)){
+    console.log("Missing size");
+}else {
+    let i = 0;
+    while (i<size){
+        console.log("x".repeat(size));
+        i++;
+    }
+}
