3b3e0efc0c131063a1a676074fd22e97cf032be2
Sophie Akoth <akothsophie9023.gmail.com>
2025-09-13 15:20:17 +0300

    correction

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,8 +1,9 @@
-const arg = process.argv[2];
+const [, , arg]= process.argv;
 const num = Number(arg);
 
-if(!isNaN(num)) {
-    console.log(`My number:${num}`);
+if(Number.isInteger(num)) {
+    console.log(`My number: ${num}`);
 }else {
     console.log("Not a number");
 }
+
