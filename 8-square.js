e0ce68832e603ad8f987df68538c61c1f2475da8
Sophie Akoth <akothsophie9023.gmail.com>
2025-09-13 16:10:56 +0300

    correction

--- 8-square.js
+++ 8-square.js
@@ -1,11 +1,11 @@
 const[, , arg] = process.argv;
 const size = Number(arg);
 
-if(!Number.isInteger(size)) {
+if (!Number.isInteger(size)) {
     console.log("Missing size");
 } else {
     let i = 0 ;
-    while(i<size) {
+    while (i <size) {
         console.log("x".repeat(size));
         i++;
     }
