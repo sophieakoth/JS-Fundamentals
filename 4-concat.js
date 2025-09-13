d8aa4bd4cb548e222be972f89c0a0c22224e6993
Sophie Akoth <akothsophie9023.gmail.com>
2025-09-13 14:24:42 +0300

    corrected the file

--- 4-concat.js
+++ 4-concat.js
@@ -1,4 +1,3 @@
-const args = process.argv.slice(2);
-if(args[0] !== undefined && args[1] !== undefined) {
-    console.log(`${args[0]}is ${args[1]}`);
-}
+const firstArg = processargv[2];
+const secondArg = processargv[3];
+console.log(`${firstArg} is ${secondArg}`);
