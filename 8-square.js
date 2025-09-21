6c7dd4ab2f022aa54dbfc4ff22f753b8096f9f36
sophieakoth <akothsophie9023@gmail.com>
2025-09-21 20:23:46 +0300

    Update 8-square.js

--- 8-square.js
+++ 8-square.js
@@ -1,13 +1,16 @@
-const[, , arg] = process.argv;
-const size = Number(arg);
+# pattern_drawing.py
+# Objective: Draw a square pattern using while and nested for loops
 
-if (!Number.isInteger(size)) {
-    console.log("Missing size");
-} else {
-    let i = 0 ;
-    while (i <size) {
-        console.log("x".repeat(size));
-        i++;
-    }
-    
-}
+# Prompt the user for pattern size
+size = int(input("Enter the size of the pattern: "))
+
+# Initialize row counter
+row = 0
+
+# Use while loop to iterate through rows
+while row < size:
+    # Use for loop to print asterisks in each row
+    for _ in range(size):
+        print("*", end="")
+    print()  # Move to the next line after each row
+    row += 1
